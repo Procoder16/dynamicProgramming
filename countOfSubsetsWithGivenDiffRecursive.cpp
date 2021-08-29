0ae66197a4d58c477d729b94b7191beda175d3f2
Procoder16 <smsoumik1608@gmail.com>
2021-08-29 12:05:16 +0530

    program to find the count of subsets with given difference using recursive apporach added

---
+++ countOfSubsetsWithGivenDiffRecursive.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countOfSubsetWithSum(int n, int arr[], int sum){
+    if(n == 0 && sum > 0){
+        return 0;
+    }
+    if(n > 0 && sum == 0){
+        return 1;
+    }
+    if(sum == 0 && n == 0){
+        return 1;
+    }
+
+    if(arr[n-1] <= sum){
+        return countOfSubsetWithSum(n-1, arr, sum - arr[n-1]) + countOfSubsetWithSum(n-1, arr, sum);
+    }
+    else{
+        return countOfSubsetWithSum(n-1, arr, sum);
+    }
+}
+
+int countOfSubsetsWithDiff(int n, int arr[], int diff){
+
+    int totalSum = 0;
+    for(int i = 0; i < n; i++){
+        totalSum+=arr[i];
+    }
+
+    int reqdSum = (diff + totalSum)/2;
+
+    int count = countOfSubsetWithSum(n, arr, reqdSum);
+
+    return count;
+}
+
+int main(){
+
+    int n;
+    cout<<"ENTER THE SIZE OF THE ARRAY:";
+    cin>>n;
+
+    int arr[n];
+
+    cout<<"ENTER THE ELEMENTS OF THE ARRAY:";
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+
+    int diff;
+    cout<<"ENTER THE DIFFERENCE THAT YOU YOU WANT TO FIND:";
+    cin>>diff;
+
+    cout<<"\nTHE COUNT OF SUBSETS WITH THE GIVEN DIFFERENCE IS:"<<countOfSubsetsWithDiff(n, arr, diff);
+
+    return 0;
+}
