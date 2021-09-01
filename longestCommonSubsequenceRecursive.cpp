5db313bd915af92e622e25c32b52eae5573961cd
Procoder16 <smsoumik1608@gmail.com>
2021-09-01 12:11:29 +0530

    logical error removed

--- longestCommonSubsequenceRecursive.cpp
+++ longestCommonSubsequenceRecursive.cpp
@@ -6,7 +6,7 @@ int lcs(string str1, string str2, int len1, int len2){
         return 0;
     }
 
-    if(str1[len1 - 1] == str2[len2 - 2]){
+    if(str1[len1 - 1] == str2[len2 - 1]){
         return 1 + lcs(str1, str2, len1 - 1, len2 - 1);
     }
     else{
