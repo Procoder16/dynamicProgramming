ef7607d51cb5f19f990f45f3324945bc587d5d00
Procoder16 <smsoumik1608@gmail.com>
2021-09-13 12:59:05 +0530

    basic format of matrix chain multiplication added

---
+++ mcmFormat.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int solve(int arr[], int i, int j){
+    //base condition
+    if(i > j){  // still depends on the solution
+        return 0;
+    }
+
+    int ans = 0;
+
+    for(int k = i; k < j; k++){
+        int temp = solve(arr, i, k) + solve(arr, k + 1, j);
+        ans = max(temp, ans);
+    }
+    
+    return ans;
+}
+
+int main(){
+
+    return 0;
+}
