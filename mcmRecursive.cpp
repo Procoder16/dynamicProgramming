59bd865dd33152d3de3568d4ae764ec50f951e3c
Procoder16 <smsoumik1608@gmail.com>
2021-09-23 13:28:01 +0530

    matrix chain multiplication recursive code added

---
+++ mcmRecursive.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int mcmRecursive(int arr[], int i, int j){
+
+    if(i >= j){
+        return 0;
+    }
+    
+    int ans = INT_MAX;
+    for(int k = i; k < j; k++){
+        int tempAns = mcmRecursive(arr, i, k) + mcmRecursive(arr, k+1, j) + (arr[i-1]*arr[k]*arr[j]);
+
+        ans = min(ans, tempAns);
+    }
+
+    return ans;
+}
+
+int main(){
+
+    int n;
+    cout<<"ENTER THE LENGTH OF THE ARRAY:";
+    cin>>n;
+
+    int arr[n];
+    cout<<"ENTER THE DIMENSIONS OF THE MATRIX IN THE ARRAY:\n";
+    for(int i = 0; i < n; i++){
+        cin>> arr[i];
+    }
+
+    cout<<"MINIMUM COST TO MULTIPLY THE MATRICES:"<<mcmRecursive(arr, 1, n-1);
+
+    return 0;
+}
